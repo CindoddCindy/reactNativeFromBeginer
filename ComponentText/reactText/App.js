c912a4415f14b42b85230d87aafcd5e179e07e11
CindoddCindy <cindodcindy@gmail.com>
2020-10-22 12:50:51 +0700

    react component text

---
+++ ComponentText/reactText/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>React Component Text</Text>
+        <Text>React Test Component Text</Text>
+        <Text>React Text Text Component</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default App;
