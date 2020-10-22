0f4abfdd367db17859a356d0c8c18a35bd6d91fb
CindoddCindy <cindodcindy@gmail.com>
2020-10-22 11:33:53 +0700

    react view compnent

---
+++ ComponentViewReact/ViewComponent/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Belajar React Nativ ......</Text>
+        <Text>Belajarr Reaccttt</Text>
+        <Text>React for Begginerr ...</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+export default App;
